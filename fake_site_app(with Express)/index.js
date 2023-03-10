0121b6894e80ed118db2e3661277f2c6a15c1dd8
quliyevnamiq8@gmail.com <quliyevnamiq8@gmail.com>
2023-03-11 00:04:00 +0400

    ..

--- fake_site_app(with Express)/index.js
+++ fake_site_app(with Express)/index.js
@@ -5,6 +5,7 @@ import path from 'path';
 import userModel from './model.js';
 import bcrypt from 'bcrypt'
 import storageMulter from './multer.js';
+import jwt from 'jsonwebtoken'
 const uri = "mongodb+srv://node01:node01@cluster0.aeevlra.mongodb.net/?retryWrites=true&w=majority";
 connect(uri)
 dotenv.config()
@@ -12,6 +13,8 @@ const app = express()
 
 app.use(express.urlencoded())
 app.use(express.static('public'))
+
+const JWT_SECRET = 'CVT_S4CR4T'
 
 app.get('/index', (req, res) => {
     res.sendFile(path.resolve('./index.html'))
@@ -37,9 +40,26 @@ app.post('/sign-up', storageMulter.single('photo'), async (req, res) => {
         age: body.age,
         email: body.email,
         password: hashedPass,
-        photo: file.originalname
+        photo: file.path
     })
-    res.send(createData)
+    res.send(`<h3>New user created! ID: ${createData._id}</h3>
+    <a href="/signin">signin</a>
+    `)
+})
+
+
+app.post('/sign-in', async (req, res) => {
+    const body = req.body
+    const nameExist = await userModel.findOne({ email: body.email })
+    if (!nameExist) {
+        res.send('Bele user yoxdur!')
+    }
+    const passRight = await bcrypt.compare(body.password, nameExist.password)
+    if (!passRight) {
+        res.send('Parol yanlisdir!')
+    }
+    const token = jwt.sign({email:body.email},JWT_SECRET)
+    res.send({ token })
 })
 
 
