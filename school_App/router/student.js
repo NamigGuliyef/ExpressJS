8943054997669005698a182efe2bcced1046385c
NamigGuliyef <quliyevnamiq8@gmail.com>
2023-03-27 16:29:30 +0400

    add validation

--- school_App/router/student.js
+++ school_App/router/student.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { allData, createdData, deletedData, getById, getByUniId, updateData } from "../controller/student.js";
+import StudentValidation from "../validation.js";
+import { validate } from 'express-validation'
 const r = Router()
 
 r.get('/', allData)
 r.get('/university/:uniId', getByUniId)
 r.get('/:Id', getById)
-r.post('/', createdData)
-r.put('/:Id', updateData)
+r.post('/', validate(StudentValidation.createOrUpdateStudent), createdData)
+r.put('/:Id', validate(StudentValidation.createOrUpdateStudent),updateData)
 r.delete('/:Id', deletedData)
 
 export default r
